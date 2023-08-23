de418c0a860b1cc9175b74e4f9029d7dd98e9319
David Corcoran <142289167+dcorcoran7@users.noreply.github.com>
2023-08-22 20:47:59 -0400

    Add files via upload

---
+++ Node.vb
@@ -0,0 +1,28 @@
+﻿Public Class Node
+    ' Dependencies: Arc
+
+    ' Node properties
+    Public Property ID As String
+    Public Property ArcsIn As List(Of Arc)
+    Public Property ArcsOut As List(Of Arc)
+    Public Property Demand As Decimal
+
+    ' constructor
+    Public Sub New()
+
+    End Sub
+
+    ' parametrized constructor
+    Public Sub New(value As String)
+        ID = value
+        ArcsIn = New List(Of Arc)
+        ArcsOut = New List(Of Arc)
+    End Sub
+
+    ' Override ToString function
+    Public Overrides Function ToString() As String
+        Dim str As String = "Node " & ID & ": Demand: " & Demand
+        Return str
+    End Function
+
+End Class
---
+++ TArc.vb
@@ -0,0 +1,25 @@
+﻿Public Class TArc
+    ' Dependencies: Arc, Node
+
+    ' Inherited Base
+    Inherits Arc
+
+    ' TArc Properties
+    Public Property PathList As New List(Of String())
+
+    ' default constructor
+    Public Sub New()
+
+    End Sub
+
+    ' parametrized constructor
+    Public Sub New(t As Node, h As Node)
+        MyBase.New(t, h)
+    End Sub
+
+    ' Override ToString function
+    Public Overrides Function ToString() As String
+        Return MyBase.ToString & ", Trans Paths: " & PathList.Count
+    End Function
+
+End Class
